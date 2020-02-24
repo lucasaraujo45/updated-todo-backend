65e82f1d2ccf569be58eb22f7ceef607b5fcd6b8
Lucas Dearaujo <lucasaraujo45@outlook.com>
2020-02-24 18:45:25 +0000

    added todo modal

--- src/models.py
+++ src/models.py
@@ -2,16 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Todos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(80), unique=False, nullable=False)
+    done = db.Column(db.Boolean, unique=False, nullable=False)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<Todos %r>' % self.text
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "done": self.done
+        }
